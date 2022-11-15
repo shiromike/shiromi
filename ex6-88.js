14863734251879d71de5c1a9d6aff859d726666e
shiromike <103973944+shiromike@users.noreply.github.com>
2022-11-15 09:36:17 +0900

    Update ex6-88.js

--- ex6-88.js
+++ ex6-88.js
@@ -1,1 +1,1 @@
-function gazo688(){document.getElementById('img1').innerHTML='<img scr="./image/ジョー君.png" art="ジョー君">';}
+function gazo688(){document.getElementById('who688').innerHTML='<img scr="./image/ジョー君.png" art="ジョー君">';}
