d9d086559966c42a1383826d2b383846cf37d1ab
shiromike <103973944+shiromike@users.noreply.github.com>
2022-12-20 10:17:39 +0900

    Update ex6-11.js

--- ex6-11.js
+++ ex6-11.js
@@ -4,7 +4,7 @@ function hyoji(){//表示する文字　
 　var cnt= document.timer.moji.value.length;
 //文字が全部表示されているか確認}
   
-  if( cnt<11 ){//現在より1文字多く切り出して表示
+  if( cnt < 11 ){//現在より1文字多く切り出して表示
   document.timer.moji.value = str.substr(0,cnt+1);}
   else{//全て表示されたら空文字に戻す
   //document.timer.moji.value = "";}}
