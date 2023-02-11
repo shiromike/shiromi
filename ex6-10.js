23ad9a22dab5d089e6fcb10949478977c23b87bd
shiromike <103973944+shiromike@users.noreply.github.com>
2023-02-11 23:15:10 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,2 +1,2 @@
 function imgchange1(){document.getElementById('logo').src ='./image/bulbon.gif';}
-function imgchange1(){document.getElementById('logo').src ='./image/josailogo.pnc';}
+function imgchange2(){document.getElementById('logo').src ='./image/josailogo.pnc';}
