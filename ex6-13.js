3ffa7538c6d4c9d383288ec8b167cfd5e58df99a
shiromike <103973944+shiromike@users.noreply.github.com>
2023-02-12 00:40:01 +0900

    Update ex6-13.js

--- ex6-13.js
+++ ex6-13.js
@@ -1,8 +1,8 @@
-function hyoji13() {　
+function hyouji13() {　
 var str13 = "一文字ずつ表示します。"}
 var cnt13 = document.timer13.moji13.value.length;
-if ( cnt < 11 ){
+if ( cnt13 < 11 ){
 document.timer13.moji13.value = str13.substr(0,cnt13+1);}
 else{
 document.timer13.moji13.value = "";}
-setTimeout("hyoji13()",1000);}
+setTimeout("hyouji13()",1000);}
