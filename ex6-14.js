22f896a58e1ba70a1a16f954b3176ca4ad72f0a3
shiromike <103973944+shiromike@users.noreply.github.com>
2023-02-11 21:24:11 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,2 +1,2 @@
-function imgchange1() {document.getElementById('logo1').src ="./image/logo.jpg";}
+function imgchange1() {document.getElementById('logo1').src ="./image/josailogo.jpg";}
 function imgchange2() {document.getElementById('logo1').src ="./image/jo-kun.jpg";}
