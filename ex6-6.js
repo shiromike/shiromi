582da52467381e152de0735d1e48cd83f67c3c62
shiromike <103973944+shiromike@users.noreply.github.com>
2023-02-11 20:29:48 +0900

    Update ex6-6.js

--- ex6-6.js
+++ ex6-6.js
@@ -3,4 +3,4 @@ function taro66(){who66 ='太郎';}
 function taro66(){who66 ='太郎'; alert( who66 +'が来た');}
 function hanako66(){who66 ='花子';}
 function hanako66(){who66 ='花子'; alert( who66 +'が来た');}
-function sayhello66(){ alert('Hello,'+ who66 +'!');}
+
