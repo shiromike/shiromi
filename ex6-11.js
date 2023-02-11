d6641aae1c962c3bdf8d6b7e9a86413c513dde47
shiromike <103973944+shiromike@users.noreply.github.com>
2023-02-12 00:38:18 +0900

    Update ex6-11.js

--- ex6-11.js
+++ ex6-11.js
@@ -1,16 +1,16 @@
 function hyouji() {
   //表示する文字　
-  var str="一文字ずつ表示します。";
+  var str = "一文字ずつ表示します。";
   
  //テキストボックスの文字数
-　var cnt= document.timer.moji.value.length;
+　var cnt = document.timer.moji.value.length;
   
 //文字が全部表示されているか確認
   
-  if( cnt < 11 ){//現在より1文字多く切り出して表示
+  if ( cnt < 11 ){//現在より1文字多く切り出して表示
   document.timer.moji.value = str.substr(0,cnt+1);}
   else{//全て表示されたら空文字に戻す
   //document.timer.moji.value = "";}}
  
-function startfnc(){//関数hyoji()を1000ミリ秒間隔で呼び出す
+function startfnc(){//関数hyouji()を1000ミリ秒間隔で呼び出す
 setlnterval("hyouji()",1000);}
